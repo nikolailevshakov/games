7265694fcd4fda66d0672113c7fda10750311bd1
Levshakov Nikolai <levshakov_nikk@bk.ru>
2020-04-04 12:16:20 +0300

    trying to create a class to get multiple obstacles

--- src/js/flyingBird.js
+++ src/js/flyingBird.js
@@ -9,7 +9,7 @@ let doAnim = true;
 let generalSpeed = 2;
 
 const player = {
-    x: 30,
+    x: 50,
     y: 210,
     dx: 0,
     dy: 0,
@@ -18,22 +18,32 @@ const player = {
     width: 50,
     height: 50
 }
-let obstacle1 = {
-    x: 500,
-    y: 0,
-    speed: generalSpeed,
-    color: 'green',
-    width: 50,
-    height: 250
+class UpObstacle {
+    constructor (x, y, width, height, speed = generalSpeed, color = 'green') {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.speed = speed;
+        this.color = color;
+    }
 }
-let obstacle2 = {
-    x: 800,
-    y: 250,
-    speed: generalSpeed,
-    color: 'green',
-    width: 50,
-    height: 250
-}
+// let obstacle1 = {
+//     x: 500,
+//     y: 0,
+//     speed: generalSpeed,
+//     color: 'green',
+//     width: 50,
+//     height: 250
+// }
+// let obstacle2 = {
+//     x: 800,
+//     y: 250,
+//     speed: generalSpeed,
+//     color: 'green',
+//     width: 50,
+//     height: 250
+// }
 // создаем игрока
 function randomColor() {
     let rand = Math.floor(Math.random()*255);
@@ -42,10 +52,6 @@ function randomColor() {
 function drawPlayer() {
     ctx.fillStyle = player.color;
     ctx.fillRect(player.x, player.y, player.width, player.height);
-}
-function drawObstacle(item) {
-    ctx.fillStyle = item.color;
-    ctx.fillRect(item.x, item.y, item.width, item.height);
 }
 
 // анимация
@@ -98,7 +104,7 @@ function keyDown(e) {
         player.dx = -player.speed;
     }
 
-    e.preventDefault();
+    // e.preventDefault();
     if(e.key === 'ArrowRight' || e.key === 'Right') {
         moveRight();
     } else if(e.key === 'ArrowLeft' || e.key ==='Left') {
@@ -126,15 +132,30 @@ function keyUp(e) {
     }
 }
 
-// двигаем препятствия и определяем столкновения
+// создаем препятствия 
 function obstacles() {
-    drawObstacle(obstacle1);
-    drawObstacle(obstacle2);
-    crashUp(obstacle1);
-    crashDown(obstacle2);
-    obstacle1.x -=obstacle1.speed;
-    obstacle2.x -=obstacle2.speed;
+    let a =500;
+    for (let i=0; i<2 ; ++i) {
+        let item = new UpObstacle(a, 0, 50, 250);
+        for (;item.x > 0;) {
+        ctx.fillStyle = item.color;
+        ctx.fillRect(item.x, item.y, item.width, item.height);
+        console.log(item);
+        item.x -= item.speed;
+        item.width -= item.speed;
+        }
+        // a += 300;
+    }
 }
+// двигаем препятствия и определяем столкновения
+// function obstacles() {
+//     drawObstacle();
+//     // drawObstacle(obstacle2);
+//     // crashUp(obstacle1);
+//     // crashDown(obstacle2);
+//     // item.x -=item.speed;
+//     // obstacle2.x -=obstacle2.speed;
+// }
 
 function crashUp(item) {
     if (player.x+player.width > item.x && 
@@ -178,9 +199,9 @@ function update() {
     drawPlayer();
     obstacles();
     newPos();
-    if (obstacle2.x + obstacle2.width<0) {
-        win();
-    }
+    // if (obstacle2.x + obstacle2.width<0) {
+    //     win();
+    // }
     requestAnimationFrame(update);   
 }
 
